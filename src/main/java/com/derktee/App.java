e7379ff4a9d978f4e80028fc8b579fd0ecf44c50
DerkT <derektan3510@gmail.com>
2022-08-27 10:23:34 -0700

    Tracked initial project code.

---
+++ src/main/java/com/derktee/App.java
@@ -0,0 +1,189 @@
+package com.derktee;
+
+/**
+ * Unit Converter
+ * @author Derek Tan
+ * @version 0.1.0
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import java.text.DecimalFormat;
+
+import javax.swing.JTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class App extends JFrame implements ActionListener {
+  // Constants
+  private static final int WINDOW_WIDTH = 320;
+  private static final int WINDOW_HEIGHT = 320;
+  private static final String WINDOW_TITLE = "UnitConverter";
+
+  private static final int UNIT_COUNT = 9;
+  private static final String DEFAULT_UNIT = "m";
+  private static final String[] UNIT_NAMES = {"m", "km", "dm", "cm", "mm", "ft", "mi", "yd", "in"};
+
+  // GUI objects
+  private JLabel sourceUnitLabel;
+  private JLabel targetUnitLabel;
+  private JTextField sourceUnitField;
+  private JTextField targetUnitField;
+
+  private JMenuBar appMenus;
+
+  private JMenu actionMenu;
+  private JMenuItem calcItem;
+  private JMenuItem resetItem;
+
+  private JMenu sourceUnitMenu;
+  private JMenuItem[] sourceUnitItems;
+  
+  private JMenu targetUnitMenu;
+  private JMenuItem[] targetUnitItems;
+
+  // other objects
+  private DecimalFormat numFormatter;
+  private LengthConverter unitConverter;
+
+  public App() {
+    // initialize form components
+    sourceUnitLabel = new JLabel("m");
+    sourceUnitField = new JTextField("0.0");
+    targetUnitLabel = new JLabel("m");
+    targetUnitField = new JTextField("0.0");
+    targetUnitField.setEditable(false);
+
+    // initialize menus
+    appMenus = new JMenuBar();
+
+    actionMenu = new JMenu("Actions");
+    calcItem = new JMenuItem("Convert");
+    resetItem = new JMenuItem("Clear");
+    
+    actionMenu.add(calcItem);
+    actionMenu.add(resetItem);
+
+    sourceUnitMenu = new JMenu("Start Unit");
+    targetUnitMenu = new JMenu("Result Unit");
+
+    sourceUnitItems = new JMenuItem[UNIT_COUNT];
+    targetUnitItems = new JMenuItem[UNIT_COUNT];
+
+    for(int i = 0; i < UNIT_COUNT; i++) {
+      sourceUnitItems[i] = new JMenuItem(UNIT_NAMES[i]);
+      sourceUnitMenu.add(sourceUnitItems[i]);
+
+      targetUnitItems[i] = new JMenuItem(UNIT_NAMES[i]);
+      targetUnitMenu.add(targetUnitItems[i]);
+    }
+
+    // initialize menu bar
+    appMenus.add(actionMenu);
+    appMenus.add(sourceUnitMenu);
+    appMenus.add(targetUnitMenu);
+
+    // initialize other objects
+    numFormatter = new DecimalFormat(".0000");
+    unitConverter = new LengthConverter(DEFAULT_UNIT);
+
+    setupApp();
+  }
+
+  private void setupApp() {
+    // 1a. populate and setup GUI layout
+    setJMenuBar(appMenus);
+    
+    add(sourceUnitLabel);
+    add(sourceUnitField);
+    add(targetUnitLabel);
+    add(targetUnitField);
+    
+    setLayout(new GridLayout(2, 2, 10, 10));
+
+    // 1b. setup listeners
+    calcItem.addActionListener(this);
+    resetItem.addActionListener(this);
+
+    for(int i = 0; i < UNIT_COUNT; i++) {
+      sourceUnitItems[i].addActionListener(this);
+      targetUnitItems[i].addActionListener(this);
+    }
+
+    // 2. prepare window
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    setTitle(WINDOW_TITLE);
+  }
+
+  public void showWindow() {
+    setVisible(true);
+  }
+
+  /**
+   * This is a helper method for validation prior to calculations!
+   * @implSpec The inputs must be non-negative double literals.
+   */
+  private boolean areValuesValid() {
+    // todo: check if the unit numbers are in a valid range > 0.0
+    return sourceUnitField.getText().charAt(0) != '-' && targetUnitField.getText().charAt(0) != '-';
+  }
+
+  private void convertValues() throws NumberFormatException, Exception {
+    unitConverter.setUnits(sourceUnitLabel.getText(), targetUnitLabel.getText());
+    
+    if (!areValuesValid())
+      throw new Exception("Invalid lengths. Cannot have negatives.");
+
+    double sourceValue = Double.parseDouble(sourceUnitField.getText());
+    
+    unitConverter.setStartValue(sourceValue);
+
+    double result = unitConverter.getConversion();
+    targetUnitField.setText(numFormatter.format(result));
+  }
+
+  private void resetValues() {
+    // reset converter state
+    unitConverter.defaultData();
+
+    // reset form
+    sourceUnitLabel.setText("m");
+    sourceUnitField.setText("0.0");
+    targetUnitLabel.setText("m");
+    targetUnitField.setText("0.0");
+  }
+
+  public void actionPerformed(ActionEvent e) {
+    Object eventTarget = e.getSource();
+    boolean shouldReset = false;
+
+    try {
+      if (eventTarget == calcItem)
+        convertValues();
+      else if (eventTarget == resetItem)
+        resetValues();
+      else if (eventTarget.getClass().getName() == "javax.swing.JMenuItem")
+        ; // TODO: implement unit setter logic!
+    } catch (NumberFormatException formatEx) {
+      System.err.println(formatEx.getMessage());
+      shouldReset = true;
+
+    } catch (Exception ex) {
+      System.err.println(ex.getMessage());
+      shouldReset = true;
+
+    } finally {
+      if (shouldReset)
+        resetValues(); // clear bad inputs on any error within conversion and formatting!
+    }
+  }
+
+  public static void main( String[] args ) {
+    App application = new App();
+    application.showWindow();
+  }
+}
---
+++ src/main/java/com/derktee/LengthConverter.java
@@ -0,0 +1,121 @@
+package com.derktee;
+
+/**
+ * This object converts from a unit named value to another unit named value. Validation is done externally in the caller, <code>App</code>.
+ * Methods of this object not only initialize its data, but also set units, determine units and conversion "directions", and do the calculations.
+ * NOTE: Unit Relation Graph below!
+ * km, dm, cm, mm <-> m <-> ft <-> mi, yd, in
+ * TODO: add dropdown menus for setting start and end unit names!
+ */
+
+import java.util.HashMap;
+
+public class LengthConverter {
+  private static final double METERS_TO_FT = 3.28084;
+
+  private String fallbackUnit;
+  private String startUnit;
+  private String endUnit;
+  private double startValue;
+
+  /**
+   * A mapping going from metric meters to other metric lengths. The graph modeled by this represents conversion routes from meters to any other metric length unit.
+   */
+  private HashMap<String, Double> metricMap;
+
+  /**
+   * Similar mapping for Imperial unit relations: feet to any other imperial length. Note that to convert from metric to imperial or vice versa, cross conversions must use the meter-ft relation. That relation uses the METERS_TO_FT factor for meters to feet!
+   */
+  private HashMap<String, Double> imperialMap;
+
+  public LengthConverter(String defaultUnit) {
+    defaultData();
+
+    // initialize metric scale: based on meters
+    metricMap.put("m", 1.0);
+    metricMap.put("km", 1000.0);
+    metricMap.put("dm", 0.1);
+    metricMap.put("cm", 0.01);
+    metricMap.put("mm", 0.001);
+
+    // initalize imperial scale: based on feet
+    imperialMap.put("ft", 1.0);
+    imperialMap.put("mi", 5280.0);
+    imperialMap.put("yd", 3.0);
+    imperialMap.put("in", 0.08333);
+  }
+
+  public void setUnits(String startUnitName, String endUnitName) {
+    startUnit = startUnitName;
+    endUnit = endUnitName;
+  }
+
+  public void setStartValue(double startVal) {
+    startValue = startVal;
+  }
+
+  public void defaultData() {
+    startUnit = fallbackUnit;
+    endUnit = fallbackUnit;
+    startValue = 0.0;
+  }
+
+  private double convImpToMetric() {
+    double toFeet = startValue * imperialMap.get(startUnit);
+    double toMeters = toFeet * (1.0 / METERS_TO_FT);
+    return toMeters * metricMap.get(endUnit);
+  }
+
+  private double convMetricToImp() {
+    double toMeters = startValue * metricMap.get(startUnit);
+    double toFeet = toMeters * METERS_TO_FT;
+    return toFeet * imperialMap.get(endUnit);
+  }
+
+  private double convertInMetric() {
+    double startFactor = metricMap.get(startUnit);
+    double endFactor = metricMap.get(endUnit);
+    double toMeters = startValue * startFactor;
+    double result = 0.0;
+
+    if (startFactor < endFactor)
+      result = toMeters / endFactor;
+    else
+      result = toMeters * endFactor;
+    
+    return result;
+  }
+
+  private double convertInImperial() {
+    double startFactor = imperialMap.get(startUnit);
+    double endFactor = imperialMap.get(endUnit);
+    double toFeet = startValue * startFactor;
+    double result = 0.0;
+
+    if (startFactor < endFactor)
+      result = toFeet / endFactor;
+    else
+      result = toFeet * endFactor;
+    
+    return result;
+  }
+
+  public double getConversion() {
+    // check direction of conversion along the unit relations
+    boolean startInImp = imperialMap.containsKey(startUnit);
+    boolean endInImp = imperialMap.containsKey(endUnit);
+    boolean startInMet = metricMap.containsKey(startUnit);
+    boolean endInMet = imperialMap.containsKey(endUnit);
+
+    if (startInImp && endInMet) // conversion cases across systems
+      return convImpToMetric();
+    else if (startInMet && endInImp)
+      return convMetricToImp();
+    else if (startInImp && endInImp) // conversion cases within a system
+      return convertInImperial();
+    else if (startInMet && endInMet)
+      return convertInMetric();
+    
+    return 0.0;
+  }
+}
